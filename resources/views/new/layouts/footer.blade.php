59617d0def1c5e4cdeb44a572e03c95f70c9b073
hendrawan-dev <hendrawan.dev@gmail.com>
2020-07-30 03:14:42 +0700

    lago

--- resources/views/new/layouts/footer.blade.php
+++ resources/views/new/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <div class="jumbotron jumbotron-fluid footer">
   <div class="container">
     <div class="row">
-      <div class="col-lg-4">
+      <div class="col-lg-3">
         <h4>Tentang</h4>
         <p>
           Kami menyediakan berbagai macam pilihan sapi, kambing dan domba berkualitas serta sesuai syariat untuk hewan
@@ -9,10 +9,18 @@
           <span>safaat.com</span>.
         </p>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-3">
+        <h4>Pengesahan</h4>
+        <p>
+          Yayasan Asy Syafaat Yang telah disahkan oleh Notaris M Zainul Arief S.H, M. Kn,
+          No. AHU.0469.AH.02.01 tanggal 28 Januari 2010
+        </p>
+      </div>
+      <div class="col-lg-3">
         <h4>Info Kontak</h4>
         <p>
-          <b>Alamat :</b> <br>
+          <b>Call Center :</b> 0813-1100-0300 <br>
+          <b>Alamat :</b>
           Yayasan Asy syafaat Almubarokah
           Masjid peradaban Asy syafaat
           Hunian islami de prima tunggulwulung kavling B06.
@@ -20,13 +28,12 @@
           Kecamatan Lowokwaru
           Kota malang. 
           <br>
-          <b>Call Center :</b> 0813-1100-0300 <br>
-          <b>Pengesahan : </b> <br>
+          {{-- <b>Pengesahan : </b> <br>
           Yayasan Asy Syafaat Yang telah disahkan oleh Notaris M Zainul Arief S.H, M. Kn, <br>
-          No. AHU.0469.AH.02.01 tanggal 28 Januari 2010
+          No. AHU.0469.AH.02.01 tanggal 28 Januari 2010 --}}
         </p>
       </div>
-      <div class="col-lg-4">
+      <div class="col-lg-3">
         <h4>Pusat Bantuan</h4>
         <a href="/faq">FAQ</a> <br>
         <a href="#">Syarat & Ketentuan</a> <br>
